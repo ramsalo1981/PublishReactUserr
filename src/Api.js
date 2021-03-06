8a99d29c638ac23f06842b2286b44d39437f1b6b
ramsalo1981 <rami.salloum@hotmail.com>
2021-03-06 17:33:33 +0100

    init project2

---
+++ src/Api.js
@@ -0,0 +1,35 @@
+//Category:
+
+//  All Categories: https://localhost:44375/api/Categories
+//  Last 4 categories with last 3 articles : https://localhost:44375/api/Categories/GetLastCategories
+//  get one category by id : https://localhost:44375/api/Categories/1
+//  get one category by id include articles: https://localhost:44375/api/Categories/GetCategoryDetails/3
+
+// Post 
+// Create category : https://localhost:44375/api/Categories
+
+//put
+// update category by id : https://localhost:44375/api/Categories/1002
+
+// Delete:
+// delete category by id : https://localhost:44375/api/Categories/1002
+
+
+
+
+
+//Article:
+
+////  All Articles: https://localhost:44375/api/Articles
+//   last 3 articles : https://localhost:44375/api/Articles/GetLastArticles
+//  get one Article by id : https://localhost:44375/api/Articles/2
+//  get one articles by id include category: https://localhost:44375/api/Articles/GetArticleDetails/2
+
+// Post 
+// Create Article : https://localhost:44375/api/Articles
+
+//put
+// update Article by id : https://localhost:44375/api/Articles/{id}
+
+// Delete:
+// delete Article by id : https://localhost:44375/api/Articles/{id}
