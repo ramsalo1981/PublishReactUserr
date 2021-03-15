f923c69e4d986a22ecc8d7f907ed129ed5c4d8d8
rami_su93 <rami.su93@hotmail.com>
2021-03-16 00:07:01 +0100

    Categories

--- src/App.js
+++ src/App.js
@@ -83,6 +83,160 @@ function App() {
                 </div>
               </div>
             </aside>
+            <aside id="ct_catsubcat_widget-2" className="widget ct_catsubcat_widget clearfix">
+              <div className="row">
+                <div className="col-lg-6">
+                  <div className="entry-cat-block" style={{backgroundColor:"#c27d23"}}>
+                    <div className="count-items">
+                      <span className="count">6</span>
+                      <div className="clear"></div>
+                      <span className="items">items</span>
+                    </div>
+                    <h3>
+                      <a href="true" title="View all posts in Wordpress">Wordpress <i className="fa fa-long-arrow-right"></i></a>
+                    </h3>
+                    <div className="entry-cat-desc">
+                      WordPress is web software you can use to create a beautiful website ...
+                    </div>
+                    <ul className="category-posts">
+                      <li>
+                        <a href="true" title="Permalink to Giving WordPress Its Own Directory"><i className="fa fa-picture"></i>Giving WordPress Its Own Directory</a>
+                      </li>
+                      <li>
+                        <a href="true" title="Permalink to Can I rename the WordPress folder?"><i className="fa fa-pencil"></i>Can I rename the WordPress folder?</a>
+                      </li>
+                      <li>
+                        <a href="true" title="Permalink to How I Can Upgrade WordPress?"><i className="fa fa-picture"></i>How I Can Upgrade WordPress?</a>
+                      </li>
+                      <li>
+                        <a href="true" title="Permalink to How WordPress Processes Post Content"><i className="fa fa-pencil"></i>How WordPress Processes Post Content</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="entry-cat-block" style={{backgroundColor:"#4e8031"}}>
+		    					  <div className="count-items">
+			    			      <span className="count">5</span>
+			    			      <div className="clear"></div>
+			    			      <span className="items">items</span>
+			    		      </div>
+
+			    	        <h3>
+                      <a href="true" title="View all posts in envato">envato <i className="fa fa-long-arrow-right"></i></a>
+                    </h3>
+
+			    				  <div className="entry-cat-desc ">Envato Support Category. Answer on Your Questions.</div>
+			              <ul className="category-posts">
+                      <li>
+                        <a href="true">
+                          <i className="fa fa-picture"></i>VirtueMart Template Submission Requirements</a>
+                      </li>
+
+			    			      <li>
+                        <a href="true" title="Permalink to PSD Templates Submission Requirements">
+                          <i className="fa fa-picture"></i>PSD Templates Submission Requirements</a>
+                      </li>
+
+                      <li>
+                        <a href="true" title="Permalink to Facebook Templates Submission Requirements">
+                          <i className="fa fa-picture"></i>Facebook Templates Submission Requirements</a>
+                      </li>
+
+			    			      <li>
+                        <a href="true" title="Permalink to General File Preparation Guidelines">
+                          <i className="fa fa-camera"></i>General File Preparation Guidelines</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+	    	        <div className="col-lg-6">
+		    	        <div className="entry-cat-block" style={{backgroundColoro: "#2daacd"}}>
+		    					  <div className="count-items">
+			    			      <span className="count">4</span>
+			    			      <div className="clear"></div>
+			    			      <span className="items">items</span>
+			    		      </div>
+		    		
+			    	        <h3>
+                      <a href="http://wp.color-theme.com/techdesk/category/microlancer" title="View all posts in microlancer">microlancer 
+                        <i className="fa fa-long-arrow-right"></i>
+                      </a>
+                    </h3>
+
+			    				  <div className="entry-cat-desc ">
+                      Microlancer is a platform which allows freelancers to create a virt ...
+                    </div>
+			
+                    <ul className="category-posts">
+                      <li>
+                        <a href="http://wp.color-theme.com/techdesk/terms-of-using-microlancer" title="Permalink to Terms of using Microlancer">
+                          <i className="fa fa-film"></i>Terms of using Microlancer</a>
+                      </li>
+
+			    			      <li>
+                        <a href="http://wp.color-theme.com/techdesk/can-buyers-of-services-resell-the-work" title="Permalink to Can buyers of services resell the work">
+                          <i className="fa fa-music"></i>Can buyers of services resell the work</a>
+                      </li>
+
+			    			      <li>
+                        <a href="http://wp.color-theme.com/techdesk/how-does-buying-a-service-work" title="Permalink to How does buying a service work?">
+                          <i className="fa fa-picture"></i>How does buying a service work?</a>
+                      </li>
+
+			    			      <li>
+                        <a href="http://wp.color-theme.com/techdesk/what-is-microlancer" title="Permalink to What is Microlancer?">
+                          <i className="fa fa-pencil"></i>What is Microlancer?</a>
+                      </li>
+
+			              </ul>
+                  </div>
+                </div>
+	    	        <div className="col-lg-6">
+		    	        <div className="entry-cat-block" style={{backgroundColor:"#40342b"}}>
+		    					  <div className="count-items">
+                      <span className="count">7</span>
+                      <div className="clear"></div>
+                      <span className="items">items</span>
+			    		      </div>
+
+			    	        <h3>
+                      <a href="http://wp.color-theme.com/techdesk/category/github" title="View all posts in github">github 
+                        <i className="fa fa-long-arrow-right"></i>
+                      </a>
+                    </h3>
+
+			    				  <div className="entry-cat-desc ">
+                      Powerful features to make software development more collaborative. ...
+                    </div>
+
+			              <ul className="category-posts">
+                      <li>
+                        <a href="http://wp.color-theme.com/techdesk/what-happens-when-i-change-my-username" title="Permalink to What happens when I change my username?">
+                          <i className="fa fa-picture"></i>What happens when I change my username?</a>
+                      </li>
+
+			    			      <li>
+                        <a href="http://wp.color-theme.com/techdesk/what-plan-should-i-choose" title="Permalink to What plan should I choose?">
+                          <i className="fa fa-picture"></i>What plan should I choose?</a>
+                      </li>
+
+			    			      <li>
+                        <a href="http://wp.color-theme.com/techdesk/how-do-i-cancel-my-account" title="Permalink to How do I cancel my account?">
+                          <i className="fa fa-music"></i>How do I cancel my account?</a>
+                      </li>
+
+			    			      <li>
+                        <a href="http://wp.color-theme.com/techdesk/what-is-my-disk-quota" title="Permalink to What is my disk quota?">
+                          <i className="fa fa-pencil"></i>What is my disk quota?</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </aside>
           </div>
           <div id="secondary" className="sidebar col-lg-4" role="complementary">
             <aside id="ct-populararticles-widget-4" className="widget ct-populararticles-widget clearfix">
